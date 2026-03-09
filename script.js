48b264eeee75036da581771db123b31231ae217a
ananyagupta4005-ctrl <ananyagupta4005@gmail.com>
2026-03-09 21:45:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function detectWaste(){
+const URL = "https://teachablemachine.withgoogle.com/models/pFSaUVG0q/";
+
+let model;
+
+async function loadModel() {
+model = await tmImage.load(URL + "model.json", URL + "metadata.json");
+}
+
+loadModel();
+
+async function detectWaste(){
+
+const imageUpload = document.getElementById("imageUpload").files[0];
+
+const img = document.createElement("img");
+img.src = window.URL.createObjectURL(imageUpload);
+
+const prediction = await model.predict(img);
+
+let highest = prediction[0];
+
+for(let i=1;i<prediction.length;i++){
+if(prediction[i].probability > highest.probability){
+highest = prediction[i];
+}
+}
 
 document.getElementById("result").innerHTML =
-"Detected Waste: Plastic Bottle → Use Blue Recycling Bin";
+"Detected Waste: " + highest.className;
 
 }
