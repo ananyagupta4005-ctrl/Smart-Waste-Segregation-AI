42932e65023f0309b6620ccce00cf6495733b05c
ananyagupta4005-ctrl <ananyagupta4005@gmail.com>
2026-03-09 22:13:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,32 +9,32 @@ async function loadModel() {
 
 loadModel();
 
-async function detectWaste() {
+async function detectWaste(){
 
-  const file = document.getElementById("imageUpload").files[0];
+    const file = document.getElementById("imageUpload").files[0];
 
-  if (!file) {
-    document.getElementById("result").innerHTML = "Please upload an image";
-    return;
-  }
-
-  const img = document.createElement("img");
-  img.src = window.URL.createObjectURL(file);
-
-  img.onload = async function () {
-
-    const prediction = await model.predict(img);
-
-    let highest = prediction[0];
-
-    for (let i = 1; i < prediction.length; i++) {
-      if (prediction[i].probability > highest.probability) {
-        highest = prediction[i];
-      }
+    if(!file){
+        document.getElementById("result").innerHTML="Please upload an image";
+        return;
     }
 
-    document.getElementById("result").innerHTML =
-      "Detected Waste: " + highest.className;
+    const img = new Image();
+    img.src = window.URL.createObjectURL(file);
 
-  };
+    img.onload = async function(){
+
+        const prediction = await model.predict(img);
+
+        let highest = prediction[0];
+
+        for(let i=1;i<prediction.length;i++){
+            if(prediction[i].probability > highest.probability){
+                highest = prediction[i];
+            }
+        }
+
+        document.getElementById("result").innerHTML =
+        "Detected Waste: " + highest.className;
+
+    }
 }
