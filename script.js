ebc6e23f5aba29ce3a80b7069ccc44e53e6407d6
ananyagupta4005-ctrl <ananyagupta4005@gmail.com>
2026-03-09 22:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,39 +3,39 @@ const URL = "https://teachablemachine.withgoogle.com/models/pFSaUVG0q/";
 let model;
 
 async function loadModel(){
-model = await tmImage.load(URL + "model.json", URL + "metadata.json");
-console.log("Model Loaded");
+    model = await tmImage.load(URL + "model.json", URL + "metadata.json");
+    console.log("Model loaded");
 }
 
 loadModel();
 
 async function detectWaste(){
 
-const file = document.getElementById("imageUpload").files[0];
+    const file = document.getElementById("imageUpload").files[0];
 
-if(!file){
-document.getElementById("result").innerHTML="Please upload an image first";
-return;
-}
+    if(!file){
+        document.getElementById("result").innerHTML="Please upload an image";
+        return;
+    }
 
-const img = new Image();
-img.src = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = window.URL.createObjectURL(file);
 
-img.onload = async function(){
+    img.onload = async function(){
 
-const prediction = await model.predict(img);
+        const prediction = await model.predict(img);
 
-let highest = prediction[0];
+        let highest = prediction[0];
 
-for(let i=1;i<prediction.length;i++){
-if(prediction[i].probability > highest.probability){
-highest = prediction[i];
-}
-}
+        for(let i=1;i<prediction.length;i++){
+            if(prediction[i].probability > highest.probability){
+                highest = prediction[i];
+            }
+        }
 
-document.getElementById("result").innerHTML =
-"Detected Waste: " + highest.className;
+        document.getElementById("result").innerHTML =
+        "Detected Waste: " + highest.className;
 
-};
+    };
 
 }
